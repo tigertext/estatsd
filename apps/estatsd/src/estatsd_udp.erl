845582fc8def5b060a615a79c554b071d2167ce2
Seth Falcon <seth@opscode.com>
2011-06-27 14:57:16 -0700

    Add batching of UDP messages to estatsd_udp
    We now collect a batch of messages before spawning a process to parse
    the packets and forward on to estatsd and folsom.  Expecting this to
    behave better during traffic bursts since we will spawn far fewer
    processes.
    
    Batches have a configurable max size and age.

--- apps/estatsd/src/estatsd_udp.erl
+++ apps/estatsd/src/estatsd_udp.erl
@@ -27,17 +27,30 @@ start_link() ->
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
--record(state, {port,
-                socket}).
+-record(state, {port          :: non_neg_integer(),
+                socket        :: inet:socket(),
+                batch = []   :: [binary()],
+                batch_size    :: non_neg_integer(),
+                batch_max     :: non_neg_integer(),
+                batch_max_age :: non_neg_integer()
+               }).
 
 init([]) ->
     {ok, Port} = application:get_env(estatsd, udp_listen_port),
     {ok, RecBuf} = application:get_env(estatsd, udp_recbuf),
+    {ok, BatchMax} = application:get_env(estatsd, udp_max_batch_size),
+    {ok, BatchAge} = application:get_env(estatsd, udp_max_batch_age),
     error_logger:info_msg("estatsd will listen on UDP ~p with recbuf ~p~n",
                           [Port, RecBuf]),
+    error_logger:info_msg("batch size ~p with max age of ~pms~n",
+                          [BatchMax, BatchAge]),
     {ok, Socket} = gen_udp:open(Port, [binary, {active, once},
                                        {recbuf, RecBuf}]),
-    {ok, #state{port = Port, socket = Socket}}.
+    {ok, #state{port = Port, socket = Socket,
+                batch = [],
+                batch_size = 0,
+                batch_max = BatchMax,
+                batch_max_age = BatchAge}}.
 
 handle_call(_Request, _From, State) ->
     {noreply, ok, State}.
@@ -45,10 +58,24 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info({udp, Socket, _Host, _Port, Bin}, State) ->
-    proc_lib:spawn(fun() -> handle_message(Bin) end),
+
+handle_info({udp, Socket, _Host, _Port, Bin},
+            State = #state{ batch_size = Size, batch_max = Max})
+  when (Size + 1) == Max ->
+    Batch = [Bin|State#state.batch],
+    proc_lib:spawn(fun() -> handle_messages(Batch) end),    
     inet:setopts(Socket, [{active, once}]),
-    {noreply, State};
+    error_logger:info_msg("spawn batch ~p FULL", [State#state.batch_size]),
+    {noreply, State#state{batch = [], batch_size = 0}};
+handle_info({udp, Socket, _Host, _Port, Bin}, State) ->
+    inet:setopts(Socket, [{active, once}]),
+    State1 = State#state{batch = [Bin|State#state.batch],
+                         batch_size = State#state.batch_size + 1},
+    {noreply, State1, State#state.batch_max_age};
+handle_info(timeout, State) ->
+    proc_lib:spawn(fun() -> handle_messages(State#state.batch) end),
+    error_logger:info_msg("spawn batch ~p TIMEOUT", [State#state.batch_size]),
+    {noreply, State#state{batch = [], batch_size = 0}};
 handle_info(_Msg, State) ->
     {noreply, State}.
 
@@ -61,6 +88,10 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+handle_messages(Batch) ->
+    [ handle_message(M) || M <- Batch ],
+    ok.
+                
 handle_message(Bin) ->
     Lines = binary:split(Bin, <<"\n">>, [global]),
     [ parse_line(L) || L <- Lines ],
