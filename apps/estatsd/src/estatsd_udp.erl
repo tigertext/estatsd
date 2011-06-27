3718516baac4a345633a7298a4cbeddb4203bab6
Kevin Smith <kevin@opscode.com>
2011-06-27 18:28:07 -0400

    Making handle_info logic more idiomatic;Processing stats in order recv'd;Making sure we set timeout everywhere

--- apps/estatsd/src/estatsd_udp.erl
+++ apps/estatsd/src/estatsd_udp.erl
@@ -30,7 +30,6 @@ start_link() ->
 -record(state, {port          :: non_neg_integer(),
                 socket        :: inet:socket(),
                 batch = []   :: [binary()],
-                batch_size    :: non_neg_integer(),
                 batch_max     :: non_neg_integer(),
                 batch_max_age :: non_neg_integer()
                }).
@@ -48,7 +47,6 @@ init([]) ->
                                        {recbuf, RecBuf}]),
     {ok, #state{port = Port, socket = Socket,
                 batch = [],
-                batch_size = 0,
                 batch_max = BatchMax,
                 batch_max_age = BatchAge}}.
 
@@ -59,23 +57,21 @@ handle_cast(_Msg, State) ->
     {noreply, State}.
 
 
-handle_info({udp, Socket, _Host, _Port, Bin},
-            State = #state{ batch_size = Size, batch_max = Max})
-  when (Size + 1) == Max ->
-    Batch = [Bin|State#state.batch],
-    proc_lib:spawn(fun() -> handle_messages(Batch) end),    
+handle_info({udp, Socket, _Host, _Port, Bin}, #state{batch=Batch,
+                                                     batch_max=Max,
+                                                     batch_max_age=MaxAge}=State) when length(Batch) == Max ->
+    error_logger:info_msg("spawn batch ~p FULL", [Max]),
+    start_batch_worker(Batch),
     inet:setopts(Socket, [{active, once}]),
-    error_logger:info_msg("spawn batch ~p FULL", [State#state.batch_size]),
-    {noreply, State#state{batch = [], batch_size = 0}};
-handle_info({udp, Socket, _Host, _Port, Bin}, State) ->
+    {noreply, State#state{batch=[Bin]}, MaxAge};
+handle_info({udp, Socket, _Host, _Port, Bin}, #state{batch=Batch,
+                                                     batch_max_age=MaxAge}=State) ->
     inet:setopts(Socket, [{active, once}]),
-    State1 = State#state{batch = [Bin|State#state.batch],
-                         batch_size = State#state.batch_size + 1},
-    {noreply, State1, State#state.batch_max_age};
-handle_info(timeout, State) ->
-    proc_lib:spawn(fun() -> handle_messages(State#state.batch) end),
-    error_logger:info_msg("spawn batch ~p TIMEOUT", [State#state.batch_size]),
-    {noreply, State#state{batch = [], batch_size = 0}};
+    {noreply, State#state{batch=[Bin|Batch]}, MaxAge};
+handle_info(timeout, #state{batch=Batch, batch_max_age=MaxAge}=State) ->
+    error_logger:info_msg("spawn batch ~p TIMEOUT", [length(Batch)]),
+    start_batch_worker(Batch),
+    {noreply, State#state{batch=[]}, MaxAge};
 handle_info(_Msg, State) ->
     {noreply, State}.
 
@@ -88,10 +84,13 @@ code_change(_OldVsn, State, _Extra) ->
 %% ------------------------------------------------------------------
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
+start_batch_worker(Batch) ->
+    %% Make sure we process messages in the order received
+    proc_lib:spawn(fun() -> handle_messages(lists:reverse(Batch)) end).
+
 handle_messages(Batch) ->
     [ handle_message(M) || M <- Batch ],
     ok.
-                
 handle_message(Bin) ->
     Lines = binary:split(Bin, <<"\n">>, [global]),
     [ parse_line(L) || L <- Lines ],
