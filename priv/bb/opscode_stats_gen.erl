f762db3f5bc83196d4240301eb483464918f2cc8
Kevin Smith <kevin@opscode.com>
2011-06-29 11:39:09 -0400

    Tucking basho_bench generators, drivers, and config into priv/bb

---
+++ priv/bb/opscode_stats_gen.erl
@@ -0,0 +1,47 @@
+-module(opscode_stats_gen).
+
+-define(TYPES, [<<"d">>, <<"c">>, <<"ms">>, <<"e">>]).
+-define(TYPE_COUNT, length(?TYPES)).
+
+-export([new_stat/1]).
+
+new_stat(_) ->
+    fun() ->
+            Name = random_name(),
+            Type = get_metric_type(Name),
+            Value = new_value(Type),
+            list_to_binary([Name, "|", Value, "|", Type]) end.
+
+new_value(Type) when Type =:= <<"d">>;
+                     Type =:= <<"c">> ->
+    integer_to_list(crypto:rand_uniform(1, 25));
+new_value(<<"ms">>) ->
+    integer_to_list(crypto:rand_uniform(1, 5000));
+new_value(<<"e">>) ->
+    integer_to_list(crypto:rand_uniform(1, 500)).
+
+random_name() ->
+    ["metric", integer_to_list(crypto:rand_uniform(1, 5000))].
+
+select_type() ->
+    lists:nth(crypto:rand_uniform(1, ?TYPE_COUNT), ?TYPES).
+
+get_metric_type(Name) ->
+    create_table(),
+    case ets:lookup(opscode_stats, Name) of
+        [] ->
+            Type = select_type(),
+            ets:insert_new(opscode_stats, {Name, Type}),
+            Type;
+        [{Name, Type}] ->
+            Type
+    end.
+
+create_table() ->
+    case catch ets:new(opscode_stats, [named_table, public]) of
+        opscode_stats ->
+            ok;
+        %% Get this if the table is already created
+        {'EXIT', {badarg, _Stacktrace}} ->
+            ok
+    end.
