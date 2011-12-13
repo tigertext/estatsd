3a54df122c9c4a096f4e41c0b058727562b8d659
Johannes Huning <johannes.huning@wooga.com>
2011-12-13 15:21:52 +0100

    Introduce module estatsd_pub
    estatsd_pub is the gen_event event manager whose handlers distribute metrics to graphing services

---
+++ apps/estatsd/src/estatsd_pub.erl
@@ -0,0 +1,16 @@
+-module (estatsd_pub).
+-export ([start_link/0]).
+
+%% @doc Starts the estatsd event manager to publish metrics via its handlers.
+start_link() ->
+  % Start the event manager itself
+  {ok, Pid} = gen_event:start_link({local, estatsd_pub}),
+  {ok, HandlerList} = application:get_env(estatsd, handler_list),
+  % Add all the handlers asked for to the event manager
+  lists:foreach(
+    fun(Handler) ->
+      gen_event:add_handler(Pid, Handler),
+      error_logger:info_msg("estatsd_pub added handler: '~s'~n", [Handler])
+    end,
+    HandlerList
+  ).
