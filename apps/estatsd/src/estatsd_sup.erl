26a13524c9d4faf6f4a01c1bcd3aa5e7cbe19477
Johannes Huning <johannes.huning@wooga.com>
2011-12-14 17:37:17 +0100

    Review estatsd_sup.erl

--- apps/estatsd/src/estatsd_sup.erl
+++ apps/estatsd/src/estatsd_sup.erl
@@ -2,36 +2,43 @@
 
 -behaviour(supervisor).
 
-%% API
--export([start_link/0]).
+%% Client API
+-export([
+  start_link/0
+]).
 
-%% Supervisor callbacks
--export([init/1]).
-
-%% ===================================================================
-%% API functions
-%% ===================================================================
+% supervisor behaviour callbacks
+-export([
+  init/1
+]).
 
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+% @doc Starts estatsd's main supervisor, registers estatsd_sup locally.
+start_link() -> supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
-%% ===================================================================
-%% Supervisor callbacks
-%% ===================================================================
 
-init([]) ->
-    Children = [
-                {estatsd_server, 
-                 {estatsd_server, start_link, []},
-                 permanent, 5000, worker, [estatsd_server]},
+% @doc supervisor callback, starts the udp- and estatsd-server.
+init(_) ->
+  ChildSpec = [
+    worker_spec_(estatsd_server),
+    worker_spec_(estatsd_udp)
+  ],
+  RestartStrategy = one_for_one,
+  % If more than MaxRestarts number of restarts occur in the last
+  % RestartTimeframe seconds, then the supervisor terminates all the child
+  % processes and then itself.
+  MaxRestarts = 10000,
+  RestartTimeframe = 10,
+  % Supervisor spec itself
+  {ok, {{RestartStrategy, MaxRestarts, RestartTimeframe}, ChildSpec}}.
 
-                {estatsd_folsom,
-                 {estatsd_folsom, start_link, []},
-                  permanent, 5000, worker, [estatsd_folsom]},
 
-                {estatsd_udp,
-                 {estatsd_udp, start_link, []},
-                  permanent, 5000, worker, [estatsd_udp]}
-               ],
-    {ok, { {one_for_one, 10000, 10}, Children} }.
+% @doc Builds a worker spec. Gives tuple-hell some semantics.
+worker_spec_(Module) ->
+  Id = Module,
+  StartFunc = {Module, start_link, []},
+  RestartType = permanent,
+  ShutdownTimeout = 5000,
+  ChildType = worker,
+  Modules = [Module],
+  {Id, StartFunc, RestartType, ShutdownTimeout, ChildType, Modules}.
