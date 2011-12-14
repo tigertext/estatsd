2e3b88a93777ca32cffce753197bbae2e929a998
Johannes Huning <johannes.huning@wooga.com>
2011-12-14 17:37:42 +0100

    Review estatsd.hrl
    Record's fields still require documentation

--- apps/estatsd/src/estatsd.hrl
+++ apps/estatsd/src/estatsd.hrl
@@ -1,6 +1,9 @@
 -type shp_metric_type() :: 'm' | 'mr' | 'g' | 'h'.
 
--record(shp_metric, {key         :: binary(),
-                     value       :: integer(),
-                     type        :: shp_metric_type(),
-                     sample_rate :: float() | undefined}).
+% TODO: Document!
+-record(shp_metric, {
+  key         :: binary(),
+  value       :: integer(),
+  type        :: shp_metric_type(),
+  sample_rate :: float() | undefined
+}).
