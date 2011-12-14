b53366e53688a30bf10f45a4109dd0b3d0f951e9
Johannes Huning <johannes.huning@wooga.com>
2011-12-14 16:58:00 +0100

    Review estatsd.erl

--- apps/estatsd/src/estatsd.erl
+++ apps/estatsd/src/estatsd.erl
@@ -1,46 +1,60 @@
 -module(estatsd).
 
+% Application Control
 -export([
-         increment/1, increment/2, increment/3,
-         decrement/1, decrement/2, decrement/3,
-         timing/2,
-         start/0, stop/0
-        ]).
+  start/0,
+  stop/0
+]).
+
+% Client API
+-export([
+  increment/1,
+  increment/2,
+  increment/3,
+  decrement/1,
+  decrement/2,
+  decrement/3,
+  timing/2
+]).
 
 -define(SERVER, estatsd_server).
 
 %% @spec start() -> ok
 %% @doc Start the estatsd server.
-start() ->
-    application:start(estatsd).
+start() -> application:start(estatsd).
+
 
 %% @spec stop() -> ok
 %% @doc Stop the estatsd server.
-stop() ->
-    application:stop(estatsd).
+stop() -> application:stop(estatsd).
 
-% Convenience: just give it the now() tuple when the work started
-timing(Key, StartTime = {_,_,_}) ->
-    Dur = erlang:round(timer:now_diff(erlang:now(), StartTime)/1000),
-    timing(Key,Dur);
 
-% Log timing information, ms
+%% @doc Log timing information given in ms.
 timing(Key, Duration) when is_integer(Duration) -> 
-    gen_server:cast(?SERVER, {timing, Key, Duration});
+  gen_server:cast(?SERVER, {timing, Key, Duration});
 
+
+%% @doc Log timing information.
 timing(Key, Duration) -> 
-    gen_server:cast(?SERVER, {timing, Key, erlang:round(Duration)}).
+  gen_server:cast(?SERVER, {timing, Key, erlang:round(Duration)}).
 
 
-% Increments one or more stats counters
+%% @doc Alias for increment(Key, 1, 1).
 increment(Key) -> increment(Key, 1, 1).
+
+%% @doc Alias for increment(Key, Amount, 1).
 increment(Key, Amount) -> increment(Key, Amount, 1).
+
+%% @doc Increments the given stat.
 increment(Key, Amount, Sample) ->
-    gen_server:cast(?SERVER, {increment, Key, Amount, Sample}).
+  gen_server:cast(?SERVER, {increment, Key, Amount, Sample}).
 
+
+%% @doc Alias for decrement(Key, -1, 1).
 decrement(Key) -> decrement(Key, -1, 1).
+
+%% @doc Alias for decrement(Key, Amount, 1).
 decrement(Key, Amount) -> decrement(Key, Amount, 1).
-decrement(Key, Amount, Sample) ->
-    increment(Key, 0 - Amount, Sample).
 
-
+%% @doc Decrements the given stat by negatively incrementing.
+decrement(Key, Amount, Sample) -> increment(Key, 0 - Amount, Sample).
