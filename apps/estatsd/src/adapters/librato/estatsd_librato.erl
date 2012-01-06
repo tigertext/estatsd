174c4c3c754eec1cf7812424a4b671cdc29e1961
Johannes Huning <johannes.huning@wooga.com>
2012-01-06 18:46:24 +0100

    Implement sending counter to Librato

--- apps/estatsd/src/adapters/librato/estatsd_librato.erl
+++ apps/estatsd/src/adapters/librato/estatsd_librato.erl
@@ -29,7 +29,44 @@ handle_metrics(Metrics, State) ->
   {ok, State}.
 
 
-%% @doc estatsd_adapter callback, sends a report to send to Librato Metrics
+%% @doc estatsd_adapter callback, sends a report to send to Librato Metrics.
 sync_handle_metrics(Metrics, State) ->
-  io:format("[~s] Pretending to send metrics to Librato...~n", [?MODULE]),
-  {ok, State, noreply}.
+  {ok, State, send_(render_(Metrics), State)}.
+
+
+%% @doc Renders recorded metrics into a message readable by Librato Metrics.
+render_({Counters, Timers}) ->
+  CountersMessage = render_counters_(Counters),
+  TimersMessage = render_timers_(Timers),
+
+  % Mochijson2 JSON struct
+  Term = {struct, [{counters, CountersMessage}, {gauges, TimersMessage}]},
+
+  % Encode the final message
+  Msg = erlang:iolist_to_binary(mochijson2:encode(Term)),
+  io:format("~s~n", [Msg]),
+  Msg.
+
+
+%% @doc Renders the counter metrics
+render_counters_(Counters) ->
+  {ok, FlushInterval} = application:get_env(estatsd, flush_interval),
+  lists:map(
+    fun({Key, {Value, Times}}) ->
+      KeyString = erlang:list_to_binary(estatsd:key2str(Key)),
+      ValuePerSec = Value / (FlushInterval / 1000),
+
+      % Build Mochijson2 JSON fragment
+      {struct, [{name, KeyString}, {value, ValuePerSec}]}
+    end,
+    Counters).
+
+
+%% @doc Renders the timer metrics
+render_timers_(Timers) -> "".  % Empty for now
+
+
+%% @doc
+send_(Message, #state{user = User, token = Token}) ->
+  % Call to the Librato Metrics API
+  librato:create_metrics(User, Token, "localhost", Message, undefined).
